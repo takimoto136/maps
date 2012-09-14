531c12f6e98d5490d265aa139257fdbb61b828af
takimoto136 <takimoto@lassic.co.jp>
2012-09-14 20:53:43 +0900

    Created rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maps::Application.config.secret_token = 'ee8d4eafea29192e03f2524dd4e70474929f780fb84b82131db2ea07c96efe698cc3b26b83d298b07cfa25857f96ab18196c7d2e2b9577a7c4076880a4a31e9a'
